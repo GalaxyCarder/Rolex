a8895cc9c5e15879edc70d3a8d99bfad9995637b
ipxkingytvip-glitch <destroyer@cyber-security.lat>
2024-06-21 12:01:01 +0000

    Update m.py

--- m.py
+++ m.py
@@ -293,7 +293,7 @@ def start_attack_reply(message, target, port, time):
 # Dictionary to store the last time each user ran the /bgmi command
 bgmi_cooldown = {}
 
-COOLDOWN_TIME =1
+COOLDOWN_TIME =0
 
 # Handler for /bgmi command
 @bot.message_handler(commands=['bgmi'])
